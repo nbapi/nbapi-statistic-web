1d9c189fa5c608faf4d4c8c1770205a0b15d0e46
zhangyang.zhu <zhangyang.zhu@corp.elong.com>
2017-12-29 13:50:08 +0800

    注释以下代码，只提交到本地，防止程序报错

--- src/main/java/com/elong/nbapi/interactive/service/InteractiveService.java
+++ src/main/java/com/elong/nbapi/interactive/service/InteractiveService.java
@@ -13,22 +13,22 @@ import com.elong.nbapi.interactive.dao.PrestoDao;
 @Service
 public class InteractiveService {
 
-	@Resource
-	private PrestoDao dao;
+//	@Resource
+//	private PrestoDao dao;
 
 	public Map<String, Object> exeSQL(String sql) {
 		if (sql != null && sql.toLowerCase().indexOf("drop") != -1) throw new RuntimeException("求不坑");
 		Map<String, Object> result = new HashMap<>();
-		List<String[]> rst = dao.exeSQL(sql);
-		if (rst == null || rst.size() == 0)
-			return result;
-		String[] title = rst.remove(0);
-		result.put("title", title);
-		result.put("data", rst);
+//		List<String[]> rst = dao.exeSQL(sql);
+//		if (rst == null || rst.size() == 0)
+//			return result;
+//		String[] title = rst.remove(0);
+//		result.put("title", title);
+//		result.put("data", rst);
 		return result;
 	}
 
-	public List<String> showTables() {
-		return dao.showTables();
-	}
+//	public List<String> showTables() {
+//		return dao.showTables();
+//	}
 }
